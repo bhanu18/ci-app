ff73f35731a6a8c1b75fb3f48548b7f27e70fb67
bhanvidh <bhanuvidh@windowslive.com>
2021-11-17 14:11:45 +0700

    new

--- app/Views/Admin/adminTemplate.php
+++ app/Views/Admin/adminTemplate.php
@@ -179,7 +179,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link">
-      <img src="/Asset/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="<?php echo site_url('public/Asset/dist/img/AdminLTELogo.png');?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -188,7 +188,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/Asset/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?php echo site_url('public/Asset/dist/img/user2-160x160.jpg');?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="<?php echo site_url('users/profile'); ?>" class="d-block"><?php echo session()->get('user_name');?></a>
