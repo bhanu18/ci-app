76144276403828016a018d7013fe3c0bbe643cea
Bhanuvidh Mansinghani <bhanuvidh@windowslive.com>
2022-01-01 13:30:39 +0700

    fixed errors

--- app/Views/addProduct.php
+++ app/Views/addProduct.php
@@ -23,36 +23,8 @@
                     <label for="exampleInputPassword1">Price</label>
                     <input type="text" class="form-control" id="exampleInputPassword1" name="Price" required>
                 </div>
-                <!-- <div class="form-group">
-                    <label for="exampleInputPassword1">Size</label>
-                    <select class="form-control" name="size" id="supplier-select" required>
-                        <option value="">Select a Size </option>
-                        <?php //if($sizes): ?>
-                            <?php //foreach($sizes as $size): ?>
-                                <option value="<?php //echo $size['id'];?>"><?php// echo $size['size'];?></option>
-                             <?php// endforeach; ?>
-                             <?php// endif; ?>   
-                    </select>
-                </div> -->
-                <!-- <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <div class="input-group">
-                        <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="exampleInputFile">
-                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                        </div>
-                        <div class="input-group-append">
-                            <span class="input-group-text">Upload</span>
-                        </div>
-                    </div>
-                </div> -->
-                <!-- <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div> -->
             </div>
             <!-- /.card-body -->
-
             <div class="card-footer">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
