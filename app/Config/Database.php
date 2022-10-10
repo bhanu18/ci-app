b83030375b0d7d5f5843164846cf267e27a95b1a
Bhanuvidh Mansinghani <bhanuvidh@windowslive.com>
2022-10-10 16:00:56 +0700

    updated config

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -30,25 +30,6 @@ class Database extends Config
 	 *
 	 * @var array
 	 */
-	// public $default = [
-	// 	'DSN'      => '',
-	// 	'hostname' => 'saledb.cmv2gp5vlvfr.ap-southeast-1.rds.amazonaws.com',
-	// 	'username' => 'admin',
-	// 	'password' => "uNique1290",
-	// 	'database' => "newapp",
-	// 	'DBDriver' => 'MySQLi',
-	// 	'DBPrefix' => '',
-	// 	'pConnect' => false,
-	// 	'DBDebug'  => (ENVIRONMENT !== 'production'),
-	// 	'charset'  => 'utf8',
-	// 	'DBCollat' => 'utf8_general_ci',
-	// 	'swapPre'  => '',
-	// 	'encrypt'  => false,
-	// 	'compress' => false,
-	// 	'strictOn' => false,
-	// 	'failover' => [],
-	// 	'port'     => 3306,
-	// ];
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'localhost',
